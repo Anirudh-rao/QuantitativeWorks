f8372019715a0859aabafce70cd8ac5d7337042a
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2026-04-02 16:07:52 +0530

    updates

--- DataIngestion/setup/IndDbscripts.sql
+++ DataIngestion/setup/IndDbscripts.sql
@@ -10,7 +10,3 @@ Create or replace table DataTable(
     Url VARCHAR(255)
 );
 
-Insert into DATATABLE (Url)
-Values
-('https://api.data.gov.in/resource/7b624b4a-1456-4945-80d0-dfb5e40ddcff?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&limit=800'),
-('https://api.data.gov.in/resource/06c9065f-70eb-4cb8-a0c0-e560291cf7f5?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&limit=20');
